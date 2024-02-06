25cd84fdd4f613b0fe16ac3b82201664ec6e96c7
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-02-06 05:03:31 -1000

    update css, html and js

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,7 +1,15 @@
 const menuBtn = document.getElementById('menu-btn');
-const menu = document.querySelector('nav');
+const menu = document.querySelector('#small-nav');
+const copyrightYear = document.getElementById('copyright-year');
+const lastModified = document.lastModified;
+const lastModifiedElement = document.getElementById('last-modified');
+const year = new Date(lastModified).getFullYear();
+
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('show');
     menuBtn.classList.toggle('show');
 })
+
+copyrightYear.textContent = year;
+lastModifiedElement.textContent = `Last Modified: ${lastModified}`;
