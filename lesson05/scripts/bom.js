535e6b9dfe53fef1cee4f2960cee5f88dbc86b87
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-02-08 12:57:38 -1000

    complete bom activity

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,25 @@
+const inputEl = document.querySelector('input');
+const btn = document.querySelector('button');
+const list = document.querySelector('#list');
+
+inputEl.focus();
+
+btn.addEventListener('click', () => {
+    if (inputEl.value != '') {
+        const li = document.createElement('li');
+        const del = document.createElement('button');
+        li.textContent = inputEl.value;
+        del.textContent = "Delete";
+        li.append(del);
+        list.append(li);
+
+        del.addEventListener('click', () => {
+            list.removeChild(li);
+            inputEl.focus;
+        })
+        
+        inputEl.value = '';
+    }
+
+    
+})
