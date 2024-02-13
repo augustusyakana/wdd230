d1133162253159588fece638b6c578355327ee3c
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-02-13 07:31:28 -1000

    added dark/light mode

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -4,7 +4,12 @@ const copyrightYear = document.getElementById('copyright-year');
 const lastModified = document.lastModified;
 const lastModifiedElement = document.getElementById('last-modified');
 const year = new Date(lastModified).getFullYear();
-
+const modeBtn = document.querySelector('#mode');
+const main = document.querySelector('main');
+const card = document.querySelectorAll('.card');
+const subcard = document.querySelectorAll('.sub-card');
+const purposeStatement = document.querySelector('#purpose');
+const footer = document.querySelector('footer');
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('show');
@@ -13,3 +18,29 @@ menuBtn.addEventListener('click', () => {
 
 copyrightYear.textContent = year;
 lastModifiedElement.textContent = `Last Modified: ${lastModified}`;
+
+modeBtn.addEventListener('click', () => {
+    if (modeBtn.textContent.includes('🌙')) {
+        main.style.background = 'linear-gradient(to right, rgb(22, 66, 22), black, rgb(22, 66, 22))';
+        footer.style.background = '#003A52';
+        footer.style.color = 'white';
+        main.style.color = 'white';
+        purposeStatement.style.color = 'white';
+        modeBtn.textContent = "☀️"
+
+        card.forEach((item) => item.style.background = "#003A52")
+        card.forEach((item) => item.style.color = 'white')
+        subcard.forEach((item) => item.style.background = '#8DAA9D')
+    } else {
+        main.style.background = 'linear-gradient(to right, rgb(81, 220, 81), white, rgb(81, 220, 81)';
+        purposeStatement.style.color = 'black';
+        footer.style.background = '#8DAA9D';
+        footer.style.color = 'black';
+        modeBtn.textContent = '🌙';
+
+        card.forEach((item) => item.style.background = "#8DAA9D")
+        card.forEach((item) => item.style.color = 'black')
+        subcard.forEach((item) => item.style.background = 'white')
+    }
+})
+
