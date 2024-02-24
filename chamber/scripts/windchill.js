39e95e0310771cc2a70ec39906149600447ad2d7
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-02-24 01:55:00 -1000

    update js & html

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -5,7 +5,7 @@ const windchill = document.getElementById('windchill');
 if (temp <= 50 && windspeed > 3) {
     let chill = 35.74 + 0.6215 * temp - 35.75 * (windspeed ** (+0.16)) + 0.4275 * (temp ** (+0.16));
     console.log(`Chill: ${chill} `);
-    windchill.textContent = chill.toFixed(2);
+    windchill.textContent = `${chill.toFixed(2)}˚ F`;
 } else {
-    console.log("nothing");
+    windchill.textContent = "N/A";
 }
