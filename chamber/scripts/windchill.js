8015366407fa3478146788cddcf627b923b03633
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-02-24 01:28:54 -1000

    added wind chill

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,11 @@
+const temp = document.getElementById('temperature').innerText;
+const windspeed = document.getElementById('windspeed').innerText;
+const windchill = document.getElementById('windchill');
+
+if (temp <= 50 && windspeed > 3) {
+    let chill = 35.74 + 0.6215 * temp - 35.75 * (windspeed ** (+0.16)) + 0.4275 * (temp ** (+0.16));
+    console.log(`Chill: ${chill} `);
+    windchill.textContent = chill.toFixed(2);
+} else {
+    console.log("nothing");
+}
