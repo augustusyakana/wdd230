2d447bbf988c4fde58c294d1aa28d74986ff4773
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-02-24 23:03:18 -1000

    added page visits functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,21 @@
 const menuBtn = document.getElementById('menu');
 const nav = document.querySelector('nav');
+const visits = document.querySelector('.visits');
 
 menuBtn.addEventListener('click', () => {
     nav.classList.toggle('show');
     menuBtn.classList.toggle('show');
 })
+
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+
+if (numVisits !== 0) {
+    visits.textContent = numVisits;
+} else {
+    visits.textContent = "This is your first visit! 🥳🎉 WELCOME!";
+}
+
+numVisits++;
+
+localStorage.setItem('numVisits-ls', numVisits);
+
