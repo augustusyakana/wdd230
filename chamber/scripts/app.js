ec152c95cb3fc6d3ff2c14cd25d3c1f86c4c674a
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-02-27 10:33:28 -1000

    added last visit functionality

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -13,6 +13,33 @@ const footer = document.querySelector('footer');
 const gallery = document.querySelector('#gallery');
 const memberPara = document.querySelector('#become-member')
 
+// variables for days visited
+const lastVisit = Number(window.localStorage.getItem('last-visit')) || Date.now();
+const visitsElement = document.querySelector('#last-visited');
+const dateToday = new Date();
+const msToDay = 86400000;
+const timeSinceLast = (Date.now() - lastVisit) / msToDay;
+console.log(timeSinceLast);
+
+if (lastVisit === dateToday.getTime()) {
+    visitsElement.textContent = 'Welcome! Let us know if you have any questions.';
+    setTime()
+} else if (timeSinceLast < 1) {
+    visitsElement.textContent = 'Back so soon! Awesome!';
+    setTime();
+} else if (timeSinceLast === 1) {
+    visitsElement.textContent = `You last visited ${lastVisit.toFixed(0)} day ago`;
+    setTime()
+} else if (timeSinceLast > 1) {
+    visitsElement.textContent = `You last visited ${timeSinceLast.toFixed(0)}`;
+    setTime();
+}
+
+function setTime() {
+    localStorage.setItem('last-visit', dateToday.getTime());
+}
+
+
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('show');
     menuBtn.classList.toggle('show');
