5527e7e07d85db1c59f475f1bd8c0b87843e44cc
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-03-20 11:28:27 -1000

    complete

--- scripts/links.js
+++ scripts/links.js
@@ -4,11 +4,30 @@ const linksURL = "https://augustusyakana.github.io/wdd230/data/links.json";
 const linksElement = document.querySelector('#links');
 const result = '';
 
-async function getLinks(url) {
-    const response = await fetch(url);
+async function getLinks() {
+    const response = await fetch(linksURL);
 
     if (response.ok) {
         const data = await response.json();
-
+        // console.log(data.lessons);
+        displayLinks(data.lessons);
     }
 }
+
+function displayLinks(lessons) {
+    lessons.forEach((lesson) => {
+        let listItem = document.createElement('li');
+        let links = lesson.links;
+        listItem.textContent = `${lesson.lesson}`;
+        links.forEach(link => {
+            let activity = document.createElement('a');
+            activity.setAttribute('href', link.url);
+            activity.textContent = ` ${link.title} |`;
+            listItem.appendChild(activity);
+            linksElement.appendChild(listItem);
+
+        })
+    })
+}
+
+getLinks();
