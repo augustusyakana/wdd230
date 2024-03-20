bee6d71498e91977ed7173de1d779a1fdb2792b5
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-03-20 10:08:09 -1000

    update

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,14 @@
 const baseURL = "https://augustusyakana.github.io/wdd230";
 const linksURL = "https://augustusyakana.github.io/wdd230/data/links.json";
 
+const linksElement = document.querySelector('#links');
+const result = '';
+
+async function getLinks(url) {
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.json();
+
+    }
+}
