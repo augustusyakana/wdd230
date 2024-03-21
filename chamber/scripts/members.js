d24bf9798a1f25fe66709d38229e51f245c75587
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-03-20 22:27:12 -1000

    update

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,4 +1,31 @@
-const main = document.querySelector('main');
+
 const membersSection = document.querySelector('#members');
 
-const membersURL = "https://augustusyakana.github.io/wdd230/chamber/data/memebers.json"
+const membersURL = "https://augustusyakana.github.io/wdd230/chamber/data/members.json";
+
+async function getMembers() {
+    const response = await fetch(membersURL);
+
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data.members);
+        displayMembers(data.members);
+    }
+}
+
+function displayMembers(members) {
+    members.forEach((member) => {
+        let card = document.createElement('div');
+        card.classList.add('card');
+
+        let company = document.createElement('h3').textContent = member.company;
+        let name = document.createElement('p').textContent = `${member.firstname} ${member.lastname}`;
+        let position = document.createElement('p').textContent = member.position;
+        let department = document.createElement('p').textContent = `Chamber Dept: ${member.department}`;
+        let ceoImg = document.createElement('img').setAttribute('src', member.photo)
+
+        console.log(department);
+    })
+}
+
+getMembers();
--- scripts/links.js
+++ scripts/links.js
@@ -2,7 +2,6 @@ const baseURL = "https://augustusyakana.github.io/wdd230";
 const linksURL = "https://augustusyakana.github.io/wdd230/data/links.json";
 
 const linksElement = document.querySelector('#links');
-const result = '';
 
 async function getLinks() {
     const response = await fetch(linksURL);
