a5ad46340d5a68baf5f0593a9ce9b6376f8060c9
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-03-20 22:08:01 -1000

    update

---
+++ chamber/scripts/members.js
@@ -0,0 +1,4 @@
+const main = document.querySelector('main');
+const membersSection = document.querySelector('#members');
+
+const membersURL = "https://augustusyakana.github.io/wdd230/chamber/data/memebers.json"
