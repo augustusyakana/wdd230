ba8044549723fc822847ffe062a951b0b6a3b202
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-03-22 08:28:09 -1000

    update

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -14,27 +14,39 @@ async function getMembers() {
 }
 
 function displayMembers(members) {
+
     members.forEach((member) => {
-        let card = document.createElement('div');
+        const card = document.createElement('div');
         card.classList.add('card');
+
+        const contactInfo = document.createElement('div');
+        const companyInfo = document.createElement('div');
+        const membershipInfo = document.createElement('div');
+        const personalInfo = document.createElement('div');
 
         let company = document.createElement('h3').textContent = member.company;
         let name = document.createElement('p').textContent = `${member.firstname} ${member.lastname}`;
-        let position = document.createElement('p').textContent = member.position;
+        let position = document.createElement('p').textContent = `Chamber Position: ${member.position}`;
         let department = document.createElement('p').textContent = `Chamber Dept: ${member.department}`;
-        // let ceoImg = document.createElement('img').setAttribute('src', member.photo);
+        let ceoImg = document.createElement('img').setAttribute('src', member.photo);
         let membership = document.createElement('p').textContent = `${member.membership} Member`;
         let email = document.createElement('p').textContent = `Email: ${member.email}`;
         let phone = document.createElement('p').textContent = `Phone: ${member.phone}`;
+        let companyEmail = document.createElement('p').textContent = `Company Email: ${member.companyEmail}`;
 
-        card.appendChild(company);
-        card.appendChild(name);
-        card.appendChild(position);
-        card.appendChild(department);
+        personalInfo.appendChild(name);
+        contactInfo.appendChild(phone);
+        contactInfo.appendChild(email);
+        companyInfo.appendChild(company);
+        companyInfo.appendChild(companyEmail);
+        membershipInfo.appendChild(position);
+        membershipInfo.appendChild(department);
+        membershipInfo.appendChild(membership);
+
+        card.appendChild(companyInfo);
+        card.appendChild(membershipInfo);
+        card.appendChild(personalInfo);
         card.appendChild(ceoImg);
-        card.appendChild(membership);
-        card.appendChild(phone);
-        card.appendChild(email);
 
         membersSection.appendChild(card);
     })
